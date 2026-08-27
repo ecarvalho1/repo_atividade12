335650ec0bb83e9fb26b83c0cd37ba39fc309b9b
eduardo <eduardo.carvalho@universo.univates.br>
2026-08-26 22:15:06 -0300

    first commit

---
+++ src/main/java/com/mycompany/atividade12/Atividade12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade12;
+
+/**
+ *
+ * @author eduardo.carvalho
+ */
+public class Atividade12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
