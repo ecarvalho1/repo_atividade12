e84953d3b4a9dfd86a27a250bdc76bf5f4e9c7b9
eduardo <eduardo.carvalho@universo.univates.br>
2026-08-26 22:18:23 -0300

    docs: alteracao da mensagem

--- src/main/java/com/mycompany/atividade12/Atividade12.java
+++ src/main/java/com/mycompany/atividade12/Atividade12.java
@@ -11,6 +11,6 @@ package com.mycompany.atividade12;
 public class Atividade12 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Boa noite!");
     }
 }
